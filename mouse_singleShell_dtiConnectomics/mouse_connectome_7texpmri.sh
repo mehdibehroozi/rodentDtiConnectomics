9c95e8833ea2602eea878f539658abe3b7ad92f7
anjeehye <anjeehye1@gmail.com>
2021-11-03 15:24:50 +0100

    voxel-res and upsampling in prepro

--- mouse_singleShell_dtiConnectomics/mouse_connectome_7texpmri.sh
+++ mouse_singleShell_dtiConnectomics/mouse_connectome_7texpmri.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # connectome reconstruction
 # Script reconstructs connectome, does filtering, writes connectivity matrix
-# requires ANO_DTI.nii atlas and ANO_DTI.txt containing all structures and label IDs of the atlas
+# requires ANO.nii atlas and ANO.txt containing all structures and label IDs of the atlas
+
 
 for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/100m.tck -seed_dynamic IN/mrtrix/wm.mif -select 100M -maxlength 30 -cutoff 0.06
-
 for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/100k.tck -seed_dynamic IN/mrtrix/wm.mif -select 100K -maxlength 30 -cutoff 0.06
+
+
+#for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/100m.tck -seed_dynamic IN/mrtrix/wm.mif -select 1M -maxlength 30 -cutoff 0.06
+#for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/100k.tck -seed_dynamic IN/mrtrix/wm.mif -select 2K -maxlength 30 -cutoff 0.06
+
 
 # cutoff optimisation, define CCROI (corpus callosum)
 #for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/5K_cutoff_XX.tck -seed_image IN/mrtrix/CCROI.mif -select 5K -maxlength 30 -cutoff XX
@@ -13,13 +18,17 @@ for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/100k.tck -seed_dynamic IN/mrtrix/
 # maxlength optimisation, define CCROI (corpus callosum)
 #for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/5K_maxlength_XX.tck -seed_image IN/mrtrix/CCROI.mif -select 5K -maxlength XX -cutoff OPTIMIZEDVALUE
 
+
 # debugging with 10000 seeds
 #for_each * : tckgen IN/mrtrix/wm.mif IN/mrtrix/10M.tck -seed_dynamic IN/mrtrix/wm.mif -select 10000 -maxlength 250 -cutoff 0.06
+
 
 
 # sift_2
 for_each * : tcksift2 IN/mrtrix/100m.tck IN/mrtrix/wm.mif IN/mrtrix/tck_weights.txt -out_mu IN/mrtrix/SIFT2_mu.txt -out_coeffs IN/mrtrix/tck_coeffs.txt
 
+# upscale atlas and convert to mif with ascending label ids, probably not needed if original atlas has no missing values in between, check later
+#for_each * : mrresize IN/rc_ix_ANO.nii -vox 0.1 -interp  IN/mrtrix/ANO_up.mif
 for_each * : cp IN/ANO_DTI.txt IN/mrtrix/atlas_lut.txt
 for_each * : labelconvert IN/ANO_DTI.nii IN/ANO_DTI.txt IN/mrtrix/atlas_lut.txt IN/mrtrix/atlas.mif
 
